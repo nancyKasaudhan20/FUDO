fae54277ffbdb608a6deb70c415a625d3f22cecc
Nancy <nancy20@gmail.com>
2024-03-27 22:35:53 +0530

    day-03

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import RestaurantCard from './RestaurantCard'
 
 const Body = () => {
   return (
-    <div>Body😘</div>
+    <div className='flex'>
+      <RestaurantCard 
+      name ="KFC" cuisines="😒🥱😶‍🌫️😛"
+      />
+      <RestaurantCard 
+      name ="Dominos" cuisines="😋😘❤️🍉"/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+    </div>
   )
 }
 
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,15 +1,17 @@
 import { CDN_URL } from "../utils/constant"
+
 const RestaurantCard = (props) => {
-    const {resData} = props;
-    const {name, cloudinaryImageId, cuisines, avgRating, sla} = resData?.info;
+  const {name, cuisines} = props;
   return (
     <>
-    <div>
-        <img alt="img" src={CDN_URL + cloudinaryImageId} />
-        <h3>{name}</h3>
+    <div className="w-28 h-60 border-0 rounded-md shadow-lg bg-slate-400 m-6 ">
+       <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf"/>
+       <h2>{name} </h2>
+       <h3>{cuisines}</h3>
+       <h4>4.5 stars</h4>
     </div>
     </>
   )
 }
 
-export default RestaurantCard
+export default RestaurantCard;
