2289db01dd51d2d620f65f8287119b91b3fb9437
Nancy <nancy20@gmail.com>
2024-03-30 16:10:10 +0530

    build search feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,9 @@ import { RESTLIST_API } from '../utils/constant'
 import { useState, useEffect } from 'react'
 const Body = () => {
   const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filterRes, setFilterRes] = useState([]);
+
   useEffect(()=>{
      fetchData();
   },[]);
@@ -13,22 +16,40 @@ const Body = () => {
      const json = await data.json();
      console.log(json);
      setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+     setFilterRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
   
-  return(ListOfRestaurants.length ===0)?(<Shimmer/>):(
+  return(ListOfRestaurants.length===0)?(<Shimmer/>):(
     <>
-    <div>
+    <div className='flex gap-20'>
+      <div className='flex'>
+          <input type='text' placeholder="search" value={searchText} 
+          onChange={(e)=>{
+            setSearchText(e.target.value)
+          }}
+          className='bg-green-300 border-spacing-1 rounded-lg '></input>
+          <button
+          onClick={()=>{
+            //filter the res cards and update the UI
+            const filteredRestaurant = ListOfRestaurants.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            )
+            // search Text
+            setFilterRes(filteredRestaurant)
+          
+          }}
+          >Search</button>
+      </div>
       <button className='p-2 bg-purple-300'
       onClick={()=>{
         const filteredList = ListOfRestaurants.filter(
           (res)=> res?.info?.avgRating > 4.2
           )
-          setListOfRestaurants(filteredList);
+          setFilterRes(filteredList);
       }}
       >Top Rated Restaurants</button>
     </div>
     <div className='flex flex-wrap'>
-    {ListOfRestaurants.map((restaurant)=>(
+    {filterRes.map((restaurant)=>(
       <RestaurantCard resData = {restaurant}
       key = {restaurant?.info?.id}
       />
